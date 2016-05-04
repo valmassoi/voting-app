b3b81a970ca647e271dd93086897d10b1e5259b2
valmassoi <rvalmassoi@gmail.com>
2016-05-04 14:04:39 -0700

    webpacked

--- public/js/pages/Dashboard.js
+++ public/js/pages/Dashboard.js
@@ -8,6 +8,30 @@ export default class Dashboard extends React.Component {
         <div class="title">
           <h1><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> Dashboard</h1>
         </div>
+        <table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Poll Name</th>
+      <th>Vote Count</th>
+      <th>Date Added</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td><Link to="poll">iPhone vs Android</Link></td>
+      <td>100</td>
+      <td>May 4, 2016</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+  </tbody>
+</table>
       </div>
     )
   }
